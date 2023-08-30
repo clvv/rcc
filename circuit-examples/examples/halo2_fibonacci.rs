e7051a9e26d959809b18cce9f33abf3161370442
dysfunctor <142473042+dysfunctor@users.noreply.github.com>
2023-08-29 17:29:10 -0700

    Add fibonacci circuit example (#2)
    * Add fibonacci circuit example
    
    * Address comments
    
    * Remove input wire `output_number`

---
+++ circuit-examples/examples/halo2_fibonacci.rs
@@ -0,0 +1,26 @@
+use rcc_halo2::builder::{H2Wire as W, *};
+
+#[main_component]
+fn my_circuit() {
+    let which_fib_number = 14;
+    assert!(which_fib_number > 3);
+
+    let first_fib_number = input_wire("first_fib_number");
+    let second_fib_number = input_wire("second_fib_number");
+
+    first_fib_number.declare_public("first_fib_number");
+    second_fib_number.declare_public("second_fib_number");
+
+    let mut fib_numbers = vec![first_fib_number, second_fib_number];
+
+    // Fibonacci constraints: fib_numbers[i] = fib_numbers[i-1] + fib_numbers[i-2]
+    //
+    for _ in 0..(which_fib_number - 2) {
+        let next_fib_number: W =
+            fib_numbers[fib_numbers.len() - 1] + fib_numbers[fib_numbers.len() - 2];
+        fib_numbers.push(next_fib_number);
+    }
+
+    let last_fib_number = *fib_numbers.last().unwrap();
+    last_fib_number.declare_public("last_fib_number");
+}
